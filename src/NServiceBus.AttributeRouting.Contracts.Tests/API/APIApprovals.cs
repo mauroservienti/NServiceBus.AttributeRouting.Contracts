0e9741c106e686d18349ddbf4ea223ba551a7dc1
Mauro Servienti <mauro.servienti@gmail.com>
2020-12-06 05:59:18 +0100

    API Approval

---
+++ src/NServiceBus.AttributeRouting.Contracts.Tests/API/APIApprovals.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using PublicApiGenerator;
+
+namespace NServiceBus.AttributeRouting.Contracts.Tests.API
+{
+    public class APIApprovals
+    {
+        [Test]
+        [UseReporter(typeof(DiffReporter))]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void Approve_API()
+        {
+            var publicApi = typeof(RouteAttribute).Assembly.GeneratePublicApi();
+
+            Approvals.Verify(publicApi);
+        }
+    }
+}
